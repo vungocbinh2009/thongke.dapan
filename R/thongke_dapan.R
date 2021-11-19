604ace0de82c8e77b2694b2f0e523addea4fc6f1
binh <vungocbinh@protonmail.com>
2021-11-19 21:30:59 +0700

    Initial commit

---
+++ R/thongke_dapan.R
@@ -0,0 +1,76 @@
+answer_estimate_mean_norm <- function () {
+
+}
+
+answer_estimate_mean_t <- function () {
+
+}
+
+answer_estimate_var <- function () {
+
+}
+
+answer_estimate_prop <- function () {
+
+}
+
+answer_sample_size_mean <- function () {
+
+}
+
+answer_sample_size_prop_1 <- function () {
+
+}
+
+answer_sample_size_prop_2 <- function () {
+
+}
+
+answer_test_mean_norm <- function () {
+
+}
+
+answer_test_mean_t <- function () {
+
+}
+
+answer_test_prop <- function () {
+
+}
+
+answer_test_goodness_of_fit <- function () {
+
+}
+
+answer_test_2_mean_norm <- function () {
+
+}
+
+answer_test_2_mean_t <- function () {
+
+}
+
+answer_test_2_prop <- function () {
+
+}
+
+answer_test_k_prop <- function () {
+
+}
+
+answer_test_independent <- function () {
+
+}
+
+answer_correlation <- function () {
+
+}
+
+answer_linear_regression <- function () {
+
+}
+
+answer_linear_regression_predict <- function () {
+
+}
+
