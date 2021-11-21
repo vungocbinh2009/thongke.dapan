129e770bc227d0f1b17e648174c92ef978fc900c
binh <vungocbinh@protonmail.com>
2021-11-21 21:57:37 +0700

    Add enter to all .mustache file

--- R/thongke_dapan.R
+++ R/thongke_dapan.R
@@ -3,13 +3,13 @@ answer_estimate_mean_norm <- function (n, mean, sigma, alpha) {
   file_name <- "./R/template/estimate/estimate_mean_norm.mustache"
   data <- estimate_mean_norm(n, mean, sigma, alpha)
   var_list <- list(
-    mean = mean,
+    mean = round(mean, 4),
     n = n,
-    s = sigma,
+    s = round(sigma, 4),
     alpha = alpha,
-    z_alpha = data$z_alpha,
-    bottom = data$bottom,
-    top = data$top
+    z_alpha = round(data$z_alpha, 4),
+    bottom = round(data$bottom, 4),
+    top = round(data$top, 4)
   )
   render_template(file_name, var_list)
 }
@@ -19,13 +19,13 @@ answer_estimate_mean_t <- function (n, mean, s, alpha) {
   file_name <- "./R/template/estimate/estimate_mean_t.mustache"
   data <- estimate_mean_t(n, mean, s, alpha)
   var_list <- list(
-    mean = mean,
+    mean = round(mean, 4),
     n = n,
-    s = s,
+    s = round(s, 4),
     alpha = alpha,
-    t_alpha = data$t_alpha,
-    bottom = data$bottom,
-    top = data$top
+    t_alpha = round(data$t_alpha, 4),
+    bottom = round(data$bottom, 4),
+    top = round(data$top, 4)
   )
   render_template(file_name, var_list)
 }
@@ -35,14 +35,14 @@ answer_estimate_var <- function (n, s, alpha) {
   data <- estimate_var(n, s, alpha)
   var_list <- list(
     n = n,
-    s = s,
+    s = round(s, 4),
     alpha = alpha,
-    chi_sq_1 = data$chi_sq_1,
-    chi_sq_2 = data$chi_sq_2,
+    chi_sq_1 = round(data$chi_sq_1, 4),
+    chi_sq_2 = round(data$chi_sq_2, 4),
     n_1 = n - 1,
-    s2 = s * s,
-    bottom = data$bottom,
-    top = data$top
+    s2 = round(s*s, 4),
+    bottom = round(data$bottom, 4),
+    top = round(data$bottom, 4)
   )
   render_template(file_name, var_list)
 }
@@ -51,14 +51,14 @@ answer_estimate_prop <- function (n, f, alpha) {
   file_name <- "./R/template/estimate/estimate_prop.mustache"
   data <- estimate_prop(n, f, alpha)
   var_list <- list(
-    k = n*f,
+    k = round(n*f, 0),
     n = n,
-    f = f,
+    f = round(f, 4),
     alpha = alpha,
-    z_alpha = data$z_alpha,
-    g = 1 - f,
-    bottom = data$bottom,
-    top = data$top
+    z_alpha = round(data$z_alpha, 4),
+    g = round(1 - f, 4),
+    bottom = round(data$bottom, 4),
+    top = round(data$top, 4)
   )
   render_template(file_name, var_list)
 }
@@ -69,9 +69,9 @@ answer_sample_size_mean <- function (sigma, eps, alpha) {
   var_list <- list(
     eps = eps,
     alpha = alpha,
-    s = sigma,
-    z_alpha = data$z_alpha,
-    value = data$value
+    s = round(sigma, 4),
+    z_alpha = round(data$z_alpha, 4),
+    value = round(data$value, 4)
   )
   render_template(file_name, var_list)
 }
@@ -80,14 +80,14 @@ answer_sample_size_prop_1 <- function (n, f, eps, alpha) {
   file_name <- "./R/template/estimate/sample_size_prop_1.mustache"
   data <- sample_size_prop_1(f, eps, alpha)
   var_list <- list(
-     k = n*f,
+     k = round(n*f, 0),
      n = n,
-     f = f,
+     f = round(f, 4),
      eps = eps,
      alpha = alpha,
-     z_alpha = data$z_alpha,
-     g = 1 - f,
-     value = data$value
+     z_alpha = round(data$z_alpha, 4),
+     g = round(1-f, 4),
+     value = round(data$value, 4)
   )
   render_template(file_name, var_list)
 }
@@ -98,8 +98,8 @@ answer_sample_size_prop_2 <- function (eps, alpha) {
   var_list <- list(
     eps = eps,
     alpha = alpha,
-    z_alpha = data$z_alpha,
-    value = data$value
+    z_alpha = round(data$z_alpha, 4),
+    value = round(data$value, 4)
   )
   render_template(file_name, var_list)
 }
@@ -109,12 +109,12 @@ answer_test_mean_norm <- function (n, mean, mean_0, sigma, alpha, mode="neq") {
   data <- test_mean_norm(n, mean, mean_0, sigma, alpha, mode)
   var_list <- list(
     mean_0 = mean_0,
-    mean = mean,
+    mean = round(mean, 4),
     n = n,
-    sigma = sigma,
-    test = data$test,
+    sigma = round(sigma, 4),
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -125,12 +125,12 @@ answer_test_mean_t <- function (n, mean, mean_0, s, alpha, mode="neq") {
   data <- test_mean_t(n, mean, mean_0, s, alpha, mode)
   var_list <- list(
     mean_0 = mean_0,
-    mean = mean,
+    mean = round(mean, 4),
     n = n,
-    s = s,
-    test = data$test,
+    s = round(s, 4),
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -140,14 +140,14 @@ answer_test_prop <- function (n, f, p_0, alpha, mode="neq") {
   file_name <- "./R/template/hypothesis_test/test_prop.mustache"
   data <- test_prop(n, f, p_0, alpha, mode)
   var_list <- list(
-    k = n*f,
+    k = round(n*f, 0),
     n = n,
-    f = f,
+    f = round(f, 4),
     p_0 = p_0,
     g_0 = 1 - p_0,
-    test = data$test,
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -158,9 +158,9 @@ answer_test_goodness_of_fit <- function (statement, actual, expected, alpha) {
   data <- test_chi_squared(actual, expected, alpha)
   var_list <- list(
     statement = statement,
-    test = data$test,
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -170,15 +170,15 @@ answer_test_2_mean_norm <- function (n1, n2, mean1, mean2, sigma1, sigma2, alpha
   file_name <- "./R/template/hypothesis_test/test_2_mean_norm.mustache"
   data <- test_2_mean_norm(n1, n2, mean1, mean2, sigma1, sigma2, alpha, mode)
   var_list <- list(
-    mean1 = mean1,
+    mean1 = round(mean1, 4),
     n1 = n1,
-    sigma1 = sigma1,
-    mean2 = mean2,
+    sigma1 = round(sigma1, 4),
+    mean2 = round(mean2, 4),
     n2 = n2,
-    sigma2 = sigma2,
-    test = data$test,
+    sigma2 = round(sigma2, 4),
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -188,16 +188,16 @@ answer_test_2_mean_t <- function (n1, n2, mean1, mean2, s1, s2, alpha, mode="neq
   file_name <- "./R/template/hypothesis_test/test_2_mean_t.mustache"
   data <- test_2_mean_t(n1, n2, mean1, mean2, s1, s2, alpha, mode)
   var_list <- list(
-    mean1 = mean1,
+    mean1 = round(mean1, 4),
     n1 = n1,
-    s1 = s1,
-    mean2 = mean2,
+    s1 = round(s1, 4),
+    mean2 = round(mean2, 4),
     n2 = n2,
-    s2 = s2,
-    s = sqrt(data$s),
-    test = data$test,
+    s2 = round(s2, 4),
+    s = round(sqrt(data$s), 4),
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -207,17 +207,17 @@ answer_test_2_prop <- function (n1, n2, f1, f2, alpha, mode="neq") {
   file_name <- "./R/template/hypothesis_test/test_2_prop.mustache"
   data <- test_2_prop(n1, n2, f1, f2, alpha, mode)
   var_list <- list(
-    k1 = n1*f1,
+    k1 = round(n1*f1, 0),
     n1 = n1,
-    f1 = f1,
-    k2 = n2*f2,
+    f1 = round(f1, 4),
+    k2 = round(n2*f2, 0),
     n2 = n2,
-    f2 = f2,
-    f = data$f,
-    g = 1 - data$f,
-    test = data$test,
+    f2 = round(f2, 4),
+    f = round(data$f, 4),
+    g = round(1 - data$f, 4),
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -228,9 +228,9 @@ answer_test_k_prop <- function (statement, m_i, n_i, alpha) {
   data <- test_n_prop(m_i, n_i, alpha)
   var_list <- list(
     statement = statement,
-    test = data$test,
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -241,9 +241,9 @@ answer_test_independent <- function (statement, matrix, alpha) {
   data <- test_independent(matrix, alpha)
   var_list <- list(
     statement = statement,
-    test = data$test,
+    test = round(data$test, 4),
     alpha = alpha,
-    c = data$c,
+    c = round(data$c, 4),
     conclusion = get_conclusion(data$test, data$c)
   )
   render_template(file_name, var_list)
@@ -269,7 +269,7 @@ answer_correlation <- function (x, y) {
     sum_x2 = data1$sum_x2,
     sum_y2 = data1$sum_y2,
     n = length(x),
-    cor = cor
+    cor = round(cor, 4)
   )
   render_template(file_name, var_list)
 }
@@ -285,10 +285,10 @@ answer_linear_regression <- function (x, y, value) {
     sum_y = data1$sum_y,
     sum_x2 = data1$sum_x2,
     n = length(x),
-    a = data2$a,
-    b = data2$b,
+    a = round(data2$a, 4),
+    b = round(data2$b, 4),
     value = value,
-    predict_value = predict_value
+    predict_value = round(predict_value, 4)
   )
   render_template(file_name, var_list)
 }
