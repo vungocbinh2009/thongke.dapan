d54bce45ee3354db2e6f0a73b30045b98723261b
binh <vungocbinh@protonmail.com>
2021-11-25 15:32:53 +0700

    Edit build.R file to remove build.sh file

--- build.R
+++ build.R
@@ -1,4 +1,24 @@
 library(devtools)
+library(here)
+
+merge_files <- function(file_list, output) {
+  output_file <- NULL
+  for(file in file_list) {
+    output_file <- c(output_file, readLines(file))
+    output_file <- c(output_file, "\n")
+  }
+  writeLines(output_file, output)
+}
+
+### BEGIN SCRIPT
+file_list <- c(
+  here("src", "estimate.R"),
+  here("src", "hypothesis_test.R"),
+  here("src", "regression.R"),
+  here("src", "util.R")
+)
+
+merge_files(file_list, here("R", "thongke_dapan.R"))
 
 build(
   pkg = ".",
@@ -11,3 +31,5 @@ build(
 )
 
 document(pkg = ".", roclets = NULL, quiet = FALSE)
+
+### END SCRIPT
