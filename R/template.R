fda59afeea2fa7d48bac44767ff4a467cca2beae
binh <vungocbinh@protonmail.com>
2022-06-26 23:06:02 +0700

    Update init_template() function

--- R/template.R
+++ R/template.R
@@ -1,8 +1,13 @@
 #' Hàm này dùng để tạo template cho việc tạo đề thi bằng thongke.dapan
 #' @export
 init_template <- function() {
-  file.create(c("exam.docx", "exam_answer.Rmd"))
-  link <- "https://raw.githubusercontent.com/vungocbinh2009/thongke.dapan/master/exam_template/test_answer_template.rmd"
-  exam_answer_content <- readLines(link)
+  exam_name <- readline(prompt = "Exam name: ")
+  dir.create(exam_name)
+  setwd(exam_name)
+  file.create("exam_answer.Rmd")
+  exam_answer_link <- "https://raw.githubusercontent.com/vungocbinh2009/thongke.dapan/master/exam_template/test_answer_template.rmd"
+  exam_answer_content <- readLines(exam_answer_link, )
   writeLines(exam_answer_content, "exam_answer.Rmd")
+  exam_word_link <- "https://github.com/vungocbinh2009/thongke.dapan/raw/master/exam_template/empty_file.docx"
+  download.file(exam_word_link, "exam.docx", quiet = TRUE)
 }
